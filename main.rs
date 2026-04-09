e74b249454f7380ff9fa6f974b3dca8928347605
fbernardes6 <fbernardessgs@gmail.com>
2026-04-09 18:18:57 -0300

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,11 +1,24 @@
+
 use std::collections::HashMap;
 
 fn main() {
-    let mut produtos = HashMap::new();
+    let mut produtos: HashMap<String, String> = HashMap::new();
 
-    produtos.insert("notebook", "Dell");
-    produtos.insert("celular", "Samsung");
-    produtos.insert("tv", "LG");
+    // Inserindo produtos
+    produtos.insert("notebook".to_string(), "Dell".to_string());
+    produtos.insert("celular".to_string(), "Samsung".to_string());
+    produtos.insert("tv".to_string(), "LG".to_string());
 
-    println!("Busca por notebook: {:?}", produtos.get("notebook"));
+    // Busca de produto
+    buscar_produto(&produtos, "notebook");
+    buscar_produto(&produtos, "celular");
+    buscar_produto(&produtos, "tablet"); // não existe
 }
+
+// Função de busca
+fn buscar_produto(produtos: &HashMap<String, String>, nome: &str) {
+    match produtos.get(nome) {
+        Some(marca) => println!("Produto encontrado: {} - Marca: {}", nome, marca),
+        None => println!("Produto '{}' não encontrado.", nome),
+    }
+}Melhoria no sistea de busca 
